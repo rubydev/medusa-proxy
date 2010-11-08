0dc74f166ab9f9e1182bdc4fc5a83f5fa6e4597b
Karel Minarik <karmi@karmi.cz>
2010-11-08 17:40:45 +0100

    Documentation & Colorization

--- proxy.rb
+++ proxy.rb
@@ -1,7 +1,21 @@
-#!/usr/bin/env ruby
+# = A simple reverse/forward proxy
+#
+# A simple proxy which forwards requests to a backend / another proxy.
+#
+# Start the proxy with command:
+#
+#   $ ruby proxy.rb
+#
+# Now connect to the internet via forwarded proxy:
+#
+#   $ curl --proxy1.0 localhost:9999 "http://example.com"
+#
 
 require 'rubygems'
 require 'em-proxy'
+require 'ansi/code'
+
+include ANSI::Code
 
 module Medusa
   class Proxy
@@ -20,7 +34,11 @@ module Medusa
   end
 end
 
-Proxy.start(:host => "0.0.0.0", :port => 9999, :debug => false) do |conn|
+host, port = '0.0.0.0', '9999'
+puts bold { "Launching proxy at #{host}:#{port}...\n" }
+
+Proxy.start(:host => host, :port => port, :debug => false) do |conn|
+
 
   # 1. Select proxy
   # TODO: random, roundrobin, balanced, ...
@@ -30,17 +48,20 @@ Proxy.start(:host => "0.0.0.0", :port => 9999, :debug => false) do |conn|
   conn.server :srv, :host => proxy.host, :port => proxy.port
 
   conn.on_data do |data|
-    p [:on_data, data]
+    puts black_on_yellow { 'on_data' } + ', request:'
+    puts data
     data
   end
 
   conn.on_response do |backend, resp|
-    p [:on_response, backend, resp]
+    puts black_on_green { 'on_response' } + " from #{ bold { backend } }, response:"
+    puts resp
     resp
   end
 
   conn.on_finish do |backend, name|
-    p [:on_finish, name]
+    puts black_on_magenta { 'on_finish' }
     unbind if backend == :srv
   end
+
 end
