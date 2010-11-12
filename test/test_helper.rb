dfbbfb2d3377493ab574a02ad0a8ff990b3d8d94
Karel Minarik <karmi@karmi.cz>
2010-11-12 11:49:52 +0100

    Added Rake task for running tests and fixed loading main gem file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
-$LOAD_PATH.unshift File.expand_path('../../', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'turn' unless ENV["TM_FILEPATH"]
 
-require 'proxy'
+require 'medusa-proxy'
